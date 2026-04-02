7e392cabf645b72fd5b536ad3cc6861624757d06
Ddos-spec <setgraph69@gmail.com>
2026-04-02 11:24:02 +0700

    Add sitemap generation and robots.txt for Google Search Console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://bengkel-las.com',
   base: '/',
   compressHTML: true,
+  integrations: [sitemap()]
 })
